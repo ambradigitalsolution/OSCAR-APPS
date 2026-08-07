30e54d1408d1cd86920b9dbc1fadaa37ad76389f
Developer <dev@oscarapp.my.id>
2026-08-07 15:46:29 +0700

    Remove dropdown arrow from user menu

--- resources/views/order_detail.blade.php
+++ resources/views/order_detail.blade.php
@@ -438,7 +438,6 @@
             <div class="user-menu">
                 <div class="avatar">O</div>
                 Admin Owner
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M6 9l6 6 6-6"/></svg>
             </div>
         </div>
 
